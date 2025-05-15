addd2eb0a618d84c87913fb23d83b989415ba86b
Santhosh Raj <156707748+sandyddeveloper@users.noreply.github.com>
2025-05-15 18:54:24 +0530

    Create factorial_of_number.sh

---
+++ SHELL/factorial_of_number.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Enter a number:"
+read num
+
+
+factorial=1
+
+
+for (( i=1; i<=num; i++ ))
+do
+  factorial=$((factorial * i))
+done
+
+
+echo "Factorial of $num is: $factorial"
